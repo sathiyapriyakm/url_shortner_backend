53efb3c29a79852445602022b697c65edf75c5c0
sathiyapriyakm <sathiyapriya.km@gmail.com>
2022-08-21 08:29:47 +0530

    Innitial commit

---
+++ index.js
@@ -0,0 +1,327 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+import cors from "cors";
+import bcrypt from "bcrypt";
+import nodemailer from "nodemailer";
+import randomstring from "randomstring";
+import {
+  createUser,
+  getUserByName,
+  getUserByEmail,
+  getUserById,
+} from "./helper.js";
+import { ObjectId } from "mongodb";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.use(express.json());
+app.use(cors());
+
+const MONGO_URL = process.env.MONGO_URL;
+async function createConnection() {
+  const client = new MongoClient(MONGO_URL);
+  await client.connect();
+  console.log("Mongo is connected ");
+  return client;
+}
+
+export const client = await createConnection();
+
+app.listen(PORT, () => console.log("Server started in port number:", PORT));
+
+async function generateHashedPassword(password) {
+  const NO_OF_ROUNDS = 10; //Number of rounds of salting
+  const salt = await bcrypt.genSalt(NO_OF_ROUNDS);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
+}
+// express.json() is a inbuilt middleware to convert data inside body to json format.
+
+function generateUrl() {
+  let genresult = "";
+  let char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charlength = char.length;
+  for (let i = 0; i < 5; i++) {
+    genresult += char.charAt(Math.floor(Math.random() * charlength));
+  }
+
+  return genresult;
+}
+
+async function check() {
+  let shorturl = generateUrl();
+  const isshortexist = await client
+    .db("guvi-node-app")
+    .collection("urlshorten")
+    .findOne({ short: shorturl });
+
+  if (isshortexist) {
+    check();
+  } else {
+    return shorturl;
+  }
+}
+
+app.get("/", function (req, res) {
+  res.send("Hello, Welcome to the APP");
+});
+
+app.post("/signup", async function (request, response) {
+  const { FirstName, LastName, Email, Password } = request.body;
+  const userFromDB = await getUserByName(Email);
+
+  if (userFromDB) {
+    response.status(400).send({ message: "Username already exists" });
+  } else {
+    const hashedPassword = await generateHashedPassword(Password);
+    //db.users.insertOne(data);
+    const result = await createUser({
+      FirstName: FirstName,
+      LastName: LastName,
+      Email: Email,
+      Password: hashedPassword,
+    });
+    // response.send({message:"User Available"});
+    const secret = process.env.SECRET_KEY;
+    const payload = {
+      Email: Email,
+    };
+    let token = jwt.sign(payload, secret, { expiresIn: "15m" });
+    //  const addtoken= await addTokenInDb(email,token)
+    const linkForUser = `${process.env.BACKEND_URL}/activatelink/${token}`;
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        // type: 'OAUTH2',
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        // clientId: process.env.OAUTH_CLIENTID,
+        // clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        // refreshToken: process.env.OAUTH_REFRESH_TOKEN
+      },
+    });
+    //Mail options
+    let mailOptions = {
+      from: "no-reply@noreply.com",
+      to: Email,
+      subject: "Activation link",
+      html: `<h4>Hello User,</h4><br><p> You can activate the account by clicking the link below.</p><br><u><a href=${linkForUser}>${linkForUser}</a></u>`,
+    };
+    //Send mail
+    transporter.sendMail(mailOptions, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("email sent successfully");
+      }
+    });
+    response.send({ message: "successful Signup" });
+  }
+});
+
+app.get("/activatelink/:token", async (request, response, next) => {
+  try {
+    let token = request.params.token;
+    jwt.verify(token, process.env.SECRET_KEY, async (err, decode) => {
+      if (decode !== undefined) {
+        const document = await client
+          .db("guvi-node-app")
+          .collection("url-shortner")
+          .findOneAndUpdate(
+            { Email: decode.Email },
+            { $set: { activate: "activate" } }
+          );
+        response.json({ message: "Account activated" });
+      } else {
+        response.status(401).json({ message: "invalid token" });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/login", async function (request, response) {
+  const { Email, Password } = request.body;
+  const userFromDB = await getUserByName(Email);
+
+  if (!userFromDB) {
+    response.status(400).send({ message: "Invalid Credential" });
+    return;
+  } else {
+    if (!userFromDB.activate) {
+      response.status(401).send({ message: "account not activated" });
+      return;
+    }
+    // check password
+    const storedPassword = userFromDB.Password;
+    const isPasswordMatch = await bcrypt.compare(Password, storedPassword);
+    if (isPasswordMatch) {
+      response.send({ message: "successful login" });
+      // localStorage.setItem("currentUser",UserName);
+    } else {
+      response.status(400).send({ message: "Invalid Credential" });
+      return;
+    }
+  }
+});
+app.post("/forgetPassword", async function (request, response) {
+  const { Email } = request.body;
+  const userFromDB = await getUserByEmail(Email);
+
+  if (!userFromDB) {
+    response.status(400).send({ message: "This is not a registered E-mail" });
+  } else {
+    //generate random string
+    let randomString = randomstring.generate();
+
+    //send a mail using nodemailer
+
+    //Create Transporter
+    const linkForUser = `${process.env.FRONTEND_URL}/reset-password/${userFromDB._id}/${randomString}`;
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        // type: 'OAUTH2',
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        // clientId: process.env.OAUTH_CLIENTID,
+        // clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        // refreshToken: process.env.OAUTH_REFRESH_TOKEN
+      },
+    });
+    //Mail options
+    let mailOptions = {
+      from: "no-reply@noreply.com",
+      to: Email,
+      subject: "Reset Password",
+      html: `<h4>Hello User,</h4><br><p> You can reset the password by clicking the link below.</p><br><u><a href=${linkForUser}>${linkForUser}</a></u>`,
+    };
+    //Send mail
+    transporter.sendMail(mailOptions, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("email sent successfully");
+      }
+    });
+    //Expiring date
+    const expiresin = new Date();
+    expiresin.setHours(expiresin.getHours() + 1);
+    //store random string
+    await client
+      .db("guvi-node-app")
+      .collection("url-shortner")
+      .findOneAndUpdate(
+        { Email: Email },
+        {
+          $set: {
+            resetPasswordToken: randomString,
+            resetPasswordExpires: expiresin,
+          },
+        }
+      );
+    //Close the connection
+    response.send({
+      message: "User exists and password reset mail is sent",
+    });
+  }
+});
+
+app.post("/verifyToken", async function (request, response) {
+  const { id, token } = request.body;
+  const userFromDB = await getUserById(id);
+  const currTime = new Date();
+  currTime.setHours(currTime.getHours());
+  try {
+    if (currTime <= userFromDB.resetPasswordExpires) {
+      if (token === userFromDB.resetPasswordToken) {
+        response.send({ message: "Changing Password Approved" });
+      } else {
+        response.status(400).send({ message: "Token not valid" });
+      }
+    } else {
+      response.status(400).send({ message: "Time expired" });
+    }
+  } catch (error) {
+    response.status(500).send({
+      message: "Something went wrong!",
+    });
+  }
+});
+
+app.put("/changePassword", async function (request, response) {
+  const { Password, id } = request.body;
+  // const userFromDB = await getUserById(id);
+  // if(!userFromDB){
+  //   response.status(400).send({message:"Invalid Credential"});
+  // }
+  // else
+  try {
+    // check password
+    const hashedPassword = await generateHashedPassword(Password);
+    await client
+      .db("guvi-node-app")
+      .collection("url-shortner")
+      .findOneAndUpdate(
+        { _id: ObjectId(id) },
+        { $set: { Password: hashedPassword } }
+      );
+    //db.users.insertOne(data);
+    response.send({ message: "Password updated successfully" });
+  } catch (error) {
+    response.send({ message: "Unexpected error in password updation" });
+  }
+});
+
+app.post("/createshorturl", async (request,response) => {
+  const {long} = request.body;
+  const isurlexist = await client
+    .db("guvi-node-app")
+    .collection("urlshorten")
+    .findOne({ long: long });
+
+  if (!isurlexist) {
+    let dt = new Date();
+    let fulldate = [];
+    let date = dt.getDate();
+    let month = dt.getMonth() + 1;
+    let year = dt.getFullYear();
+
+    fulldate.push(date);
+    fulldate.push(month);
+    fulldate.push(year);
+    let short = await check();
+    const result = await client
+      .db("guvi-node-app")
+      .collection("urlshorten")
+      .insertMany([
+        {
+          long: long,
+          short: short,
+          visit: 0,
+          createdAt: [{ date: date, month: month, year: year }],
+        },
+      ]);
+      console.log(result);
+      response.send({ message: "URL generated" });
+      return;
+  } else {
+    console.log("url already exist");
+    response.status(400).send({message: `url already exist kindly refer show Table and the URL is: ${isurlexist.short}`});
+    return;
+  }
+});
+
+app.get("/geturl", async (request, response) => {
+  const filter = request.query;
+  const result = await client
+    .db("guvi-node-app")
+    .collection("urlshorten")
+    .find(filter)
+    .toArray();
+  response.send(result);
+});
